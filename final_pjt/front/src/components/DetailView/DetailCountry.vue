4baacf946590ed3a3a7b30832adf7ffdd398d334
jhpark <brianwogus@naver.com>
2024-05-21 15:06:20 +0900

    detailView 틀 완성

---
+++ final_pjt/front/src/components/DetailView/DetailCountry.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>{{ data.name }}</h3>
+    <img
+    :src="imgUrl(data.image1)" alt="" class="resized-image">
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+import { ref } from 'vue'
+
+const store = useCounterStore()
+
+const imgUrl = store.getImageUrl
+
+defineProps({
+  data: Object
+})
+
+</script>
+
+<style scoped>
+
+.resized-image {
+  width: 100%;
+  height: auto;
+}
+</style>
---
+++ final_pjt/front/src/components/DetailView/DetailGraph.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrap">
+    <div class="detail-content">
+      <img :src="imgUrl(data.graph)" alt="" class="resized-image">
+      <div>
+        <p>{{ burger }}</p>
+        <p>{{ coffee }}</p>
+      </div>
+    </div>
+    <div>
+      <DetailComment />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+import { ref } from 'vue'
+import DetailComment from '@/components/DetailView/DetailComment.vue'
+
+const store = useCounterStore()
+
+const imgUrl = store.getImageUrl
+
+const props = defineProps({
+  data: Object,
+})
+
+const burger = (Math.round(props.data.burger * props.data.rate / 10)) * 10
+const coffee = (Math.round(props.data.coffee * props.data.rate / 10)) * 10
+
+</script>
+
+<style scoped>
+
+.detail-content {
+  display: flex;
+  align-items: center; /* 이미지를 세로로 가운데 정렬합니다. */
+}
+
+.resized-image {
+  width: 80%; /* 최대 너비를 50%로 설정합니다. */
+  height: auto; /* 높이는 비율에 맞게 자동 조절됩니다. */
+}
+
+.wrap {
+  display: flex;
+  flex-direction: column;
+}
+</style>
